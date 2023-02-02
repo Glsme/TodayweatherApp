f2f67d421f375ca10bd8e235628bfc3458138981
Glsme <glasses.str.man@gmail.com>
2023-02-02 22:21:36 +0900

    Initial Commit

---
+++ TodayweatherApp/TodayweatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodayweatherAppApp.swift
+//  TodayweatherApp
+//
+//  Created by Seokjune Hong on 2023/02/02.
+//
+
+import SwiftUI
+
+@main
+struct TodayweatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
