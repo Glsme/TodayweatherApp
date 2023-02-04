13f5e690a248a4a1237b5605a4726f363f6f011d
Glsme <glasses.str.man@gmail.com>
2023-02-04 22:29:09 +0900

    feat: WeatherAPIManager 파일 추가

---
+++ TodayweatherApp/Network/WeatherAPIManager.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherAPIManager.swift
+//  TodayweatherApp
+//
+//  Created by Seokjune Hong on 2023/02/04.
+//
+
+import Foundation
+
+final class WeatherAPIManager {
+    static let shared = WeatherAPIManager()
+    
+    private init() { }
+}
