ae6e74506d373de942bb0ad15aaa8c674e349262
Glsme <glasses.str.man@gmail.com>
2023-02-14 23:23:33 +0900

    Feat: iOS 16 Widget 제거(다이나믹 아일랜드)

--- WeatherWidget/WeatherWidget.swift
+++ WeatherWidget/WeatherWidget.swift
@@ -8,17 +8,21 @@
 import WidgetKit
 import SwiftUI
 import Intents
+import Combine
 
 struct Provider: IntentTimelineProvider {
+    // 특정 내용이 없는 시각적 표현
     func placeholder(in context: Context) -> SimpleEntry {
         SimpleEntry(date: Date(), configuration: ConfigurationIntent())
     }
 
+    // WidgetKit이 위젯이 일시적인 상황에 나타나면 호출하는 함수
     func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
         let entry = SimpleEntry(date: Date(), configuration: configuration)
         completion(entry)
     }
 
+    // 현재 시간과 위젯을 업데이트할 향후 시간에 대한 타임라인 항목을 제공
     func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         var entries: [SimpleEntry] = []
 
@@ -41,10 +45,12 @@ struct SimpleEntry: TimelineEntry {
 }
 
 struct WeatherWidgetEntryView : View {
+    @Environment(\.widgetFamily) var family: WidgetFamily
     var entry: Provider.Entry
 
     var body: some View {
-        Text(entry.date, style: .time)
+        Image(systemName: "person")
+        Text("안녕하세요")
     }
 }
 
