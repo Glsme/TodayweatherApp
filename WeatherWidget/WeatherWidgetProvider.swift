463a03ae1d905c720ab0c44652a2759785267780
Glsme <glasses.str.man@gmail.com>
2023-04-02 21:58:00 +0900

    Refactor: Widget SimpleEntry Structure 이름 변경 및 적용

--- WeatherWidget/WeatherWidgetProvider.swift
+++ WeatherWidget/WeatherWidgetProvider.swift
@@ -14,32 +14,31 @@ import CoreLocation
 struct Provider: IntentTimelineProvider {
     let viewModel = WidgetViewModel()
     // 특정 내용이 없는 시각적 표현
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), image: "moon", text: "")
+    func placeholder(in context: Context) -> WeatherData {
+        WeatherData(date: Date(), text: "")
     }
     
     // WidgetKit이 위젯이 일시적인 상황에 나타나면 호출하는 함수
-    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), image: "moon", text: "")
+    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (WeatherData) -> ()) {
+        let entry = WeatherData(date: Date(), text: "")
         completion(entry)
     }
     
     // 현재 시간과 위젯을 업데이트할 향후 시간에 대한 타임라인 항목을 제공
-    func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<SimpleEntry>) -> ()) {
+    func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<WeatherData>) -> ()) {
         viewModel.getLocation { result in
             // Generate a timeline consisting of five entries an hour apart, starting from the current date.
             let refreshTime = result.bool ? 20 : 3
             let currentDate = Date()
             let entryDate = Calendar.current.date(byAdding: .minute, value: refreshTime, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, image: "moon", text: "\(result.value)")
+            let entry = WeatherData(date: entryDate, text: "\(result.value)")
             let timeline = Timeline(entries: [entry], policy: .atEnd)
             completion(timeline)
         }
     }
 }
 
-struct SimpleEntry: TimelineEntry {
+struct WeatherData: TimelineEntry {
     let date: Date
-    let image: String
     let text: String
 }
