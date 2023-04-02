92c75f210a36138c83c78152caba3e8b9c6b4558
Glsme <glasses.str.man@gmail.com>
2023-04-02 22:12:42 +0900

    Refactor: Widget 폴더 추가 및 적용

---
+++ WeatherWidget/Model/WeatherData.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherData.swift
+//  TodayweatherApp
+//
+//  Created by Seokjune Hong on 2023/04/02.
+//
+
+import Foundation
+import WidgetKit
+
+struct WeatherData: TimelineEntry {
+    let date: Date
+    let text: String
+}
